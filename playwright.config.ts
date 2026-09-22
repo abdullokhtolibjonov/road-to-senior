cdb234eb9e0993cd301df87649f637d5fa39ef48
abdullokhtolibjonov <abdullokh.tolibjonov@gmail.com>
2026-09-22 18:15:43 +0500

    phase 2 done

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30_000,
+  expect: {
+    timeout: 5_000
+  },
+  fullyParallel: true,
+  reporter: 'html',
+  
+
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+    //headless: false,
+    testIdAttribute: 'data-test'
+  },
+
+  projects: [{
+    name: 'chromium',
+    use: { ...devices['Desktop Chrome'] },
+  }]
+})
